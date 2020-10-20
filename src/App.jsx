d32f5ffa197f5452511f3f59a3a9455399a488c6
tohshine <tohshine@gmail.com>
2020-10-20 17:20:46 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ function App() {
 
           <Route path='/getstarted' exact component={New} />
 
-          <Route path='/' exact>
+          <Route path='/'exact>
             <LandingPage />
           </Route>
         </Switch>
